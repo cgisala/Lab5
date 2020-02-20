4b8980f43c6f9818b5aed5f5b6cd75dd619c4d6f
chris <chris@chris-pc>
2020-02-20 12:21:57 -0600

    Deleted unnecessary files

--- Examples/cat.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = 'https://catfact.ninja/fact'
-
-cat_data = requests.get(url).json()
-
-print(cat_data)
-print(cat_data['fact'])
--- Examples/coindex.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
-
-coin_data = requests.get(url).json()
-
-print(coin_data)
-# print(coin_data['bpi']['USD'])
