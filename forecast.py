dbc3752ac7fd09a3c2ec367bd299f8539a5d03bb
chris <chris@chris-pc>
2020-02-20 20:01:31 -0600

    Added ability to get forecast weather for a specific US city

--- forecast.py
+++ forecast.py
@@ -3,9 +3,16 @@ import os
 from datetime import datetime
 from pprint import pprint
 
+
+
 # Variables
-key = os.environ.get('WEATHER_KEY')
-query = {'q': 'minneapolis,us', 'units': 'imperial', 'appid': key}
+# key = os.environ.get('WEATHER_KEY')
+key = '931ff5eff05bb2caa4f58e70a64f78bb'
+
+city = input('\nWhat US city do you want to see the weather: ')
+location = f'{city},us'
+query = {'q': location, 'units': 'imperial', 'appid': key}
+# query = {'q': 'minneapolis,us', 'units': 'imperial', 'appid': key}
 
 url = 'https://api.openweathermap.org/data/2.5/forecast'
 data = requests.get(url, params=query).json()
@@ -16,7 +23,7 @@ for forecast in forecast_items:
     timestamp = forecast['dt'] #Unix timestamp
     date = datetime.fromtimestamp(timestamp) # Convert to a datetime, for humans
     temp = forecast['main']['temp']
-    print(f'at{date} temp is {temp}')
+    print(f'\nat{date} temp is {temp}')
 
 
 
